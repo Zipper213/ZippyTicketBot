96233a96c50868b5d4b80fde0635bae2463f582e
Zipper213 <varholikb@yahoo.com>
2019-03-20 12:39:15 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+const Prefix = "[";
+
+Client.on('ready', () => {
+	console.log('Bot Ready!');
+});
+
+Client.on('message', (message) => {
+
+	let msgP = msg.content
+	let msg = msg.substring(1);
+	let author = message.author;
+	let channel = message.channel;
+
+	if(!msgP.startsWith(Prefix)) return;
+
+	if(msg === "test") {
+		channel.send("IT WORKS!");
+	}
+})
+
+Client.login(process.env.BOT_TOKEN);
